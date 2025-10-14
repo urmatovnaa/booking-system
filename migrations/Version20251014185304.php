537bd76674e1a3bc9a7e5c2bbf46165bbd4e02c9
Altynai <urmatovnaa05@gmail.com>
2025-10-14 21:05:44 +0200

    add migrations

---
+++ migrations/Version20251014185304.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251014185304 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, resource_id INT NOT NULL, INDEX IDX_E00CEDDE89329D25 (resource_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE89329D25 FOREIGN KEY (resource_id) REFERENCES resource (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE89329D25');
+        $this->addSql('DROP TABLE booking');
+    }
+}
